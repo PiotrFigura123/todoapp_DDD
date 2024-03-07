47305e09fb688e51095406ae862ae29dcc132b86
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2024-03-07 11:00:16 +0100

    init

---
+++ src/test/java/pl/piotrFigura/ToDoApp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piotrFigura.ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
