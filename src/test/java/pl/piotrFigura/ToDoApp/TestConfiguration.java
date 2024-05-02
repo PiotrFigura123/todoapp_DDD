d78373ba65ab649137fae468a1e1454a2d6f7145
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2024-05-02 15:46:22 +0200

    @Profile

---
+++ src/test/java/pl/piotrFigura/ToDoApp/TestConfiguration.java
@@ -0,0 +1,50 @@
+package pl.piotrFigura.ToDoApp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import pl.piotrFigura.ToDoApp.task.domain.Task;
+import pl.piotrFigura.ToDoApp.task.infrastructure.jpa.TaskRepository;
+
+@Configuration
+class TestConfiguration {
+
+    @Bean
+    @Profile("integration")
+    TaskRepository testRepo() {
+        return new TaskRepository() {
+
+            private Map<Long, Task> tasks = new HashMap<>();
+
+            @Override
+            public Optional<Task> findById(Long id) {
+                return Optional.ofNullable(tasks.get(id));
+            }
+
+            @Override
+            public Task save(Task source) {
+                return tasks.put(tasks.size() + 1l, source);
+            }
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(tasks.values());
+            }
+
+            @Override
+            public boolean existsById(Long id) {
+                return tasks.containsKey(id);
+            }
+
+            @Override
+            public boolean existsByTaskGroup(boolean isDone, Long groupId) {
+                return false;
+            }
+        };
+    }
+}
---
+++ src/test/java/pl/piotrFigura/ToDoApp/task/infrastructure/jpa/TaskRepositoryImplTest.java
@@ -0,0 +1,37 @@
+package pl.piotrFigura.ToDoApp.task.infrastructure.jpa;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import pl.piotrFigura.ToDoApp.task.domain.Task;
+
+public class TaskRepositoryImplTest implements TaskRepository{
+
+    private Map<Long, Task> tasks = new HashMap<>();
+    @Override
+    public Optional<Task> findById(Long id) {
+        return Optional.ofNullable(tasks.get(id));
+    }
+
+    @Override
+    public Task save(Task source) {
+        return tasks.put(tasks.size() + 1l, source);
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return tasks.containsKey(id);
+    }
+
+    @Override
+    public boolean existsByTaskGroup(boolean isDone, Long groupId) {
+        return false;
+    }
+}
