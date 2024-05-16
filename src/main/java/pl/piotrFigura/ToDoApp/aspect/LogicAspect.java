1271cc03f5ec79c6989f5df45b2dd17c2e98b01f
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2024-05-16 15:02:04 +0200

    @Pointcut, @Before

--- src/main/java/pl/piotrFigura/ToDoApp/aspect/LogicAspect.java
+++ src/main/java/pl/piotrFigura/ToDoApp/aspect/LogicAspect.java
@@ -2,21 +2,35 @@ package pl.piotrFigura.ToDoApp.aspect;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
+@Slf4j
 @Component
 class LogicAspect {
+
     private final Timer projectCreateGroupTimer;
 
     LogicAspect(final MeterRegistry registry) {
         projectCreateGroupTimer = registry.timer("logic.product.create.group");
     }
+    @Pointcut("execution(* pl.piotrFigura.ToDoApp.project.infrastructure.ProjectService.createGroup(..))")
+    static void projectServiceCreateGroup(){
 
-    @Around("execution(* pl.piotrFigura.ToDoApp.project.infrastructure.ProjectService.createGroup(..))")
+    }
+    @Before("projectServiceCreateGroup()")
+    void logMethodCall(JoinPoint joinPoint){
+            log.info("Before {} with {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
+    }
+
+    @Around("projectServiceCreateGroup()")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp){
         return projectCreateGroupTimer.record(() -> {
             try {
