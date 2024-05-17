bed35321589c9ef08ddf59ec366ef05b4046acf3
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2024-05-17 12:13:33 +0200

    @Async

--- src/main/java/pl/piotrFigura/ToDoApp/reports/ChangedTaskEventListener.java
+++ src/main/java/pl/piotrFigura/ToDoApp/reports/ChangedTaskEventListener.java
@@ -2,6 +2,7 @@ package pl.piotrFigura.ToDoApp.reports;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import pl.piotrFigura.ToDoApp.event.TaskDone;
 import pl.piotrFigura.ToDoApp.event.TaskEvent;
@@ -17,13 +18,14 @@ class ChangedTaskEventListener {
         this.repository = repository;
     }
 
+    @Async
     @EventListener
     void on(TaskDone event){
         log.info("Got " + event);
         onChanged(event);
     }
 
-
+    @Async
     @EventListener
     void on(TaskUndone event){
         log.info("Got " + event);
